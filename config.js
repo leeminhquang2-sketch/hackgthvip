f4b426e1398afa49cf28e2e43aa57883cf8d0b9b
leeminhquang2-sketch <leeminhquang2@gmail.com>
2026-06-05 22:26:15 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    // ============== ADMIN (UID Facebook của bạn) ==============
+    admins: ["61567536611221"],
+    
+    // ============== PREFIX (Ký tự đầu lệnh) ==============
+    prefix: "!",
+    
+    // ============== TÊN BOT ==============
+    botName: "ByMquangCte",
+    
+    // ============== CẤU HÌNH GAME ==============
+    gameIP: "211.253.26.47",
+    gamePort: "8093",
+    
+    // ============== KEYS AES (KHÔNG ĐỔI) ==============
+    aesKey: "gksekfidjrqjfwk1",
+    aesIV: "towerdefense_amo",
+    
+    // ============== DANH SÁCH SERVER ==============
+    servers: {
+        "1": { name: "AMO", display: "AMO (Mobile)" },
+        "2": { name: "ATV", display: "ATV (Android TV)" }
+    },
+    
+    // ============== DANH SÁCH SOUL ==============
+    souls: {
+        "1": { id: "SOUL_1", name: "Loki" },
+        "2": { id: "SOUL_2", name: "Gibongi" },
+        "3": { id: "SOUL_3", name: "Hell Knight" },
+        "4": { id: "SOUL_4", name: "Nan-cheon" },
+        "5": { id: "SOUL_5", name: "Dalma" },
+        "6": { id: "SOUL_6", name: "Muk-hyang" },
+        "7": { id: "SOUL_7", name: "Wolf King" },
+        "8": { id: "SOUL_8", name: "Jormungand" }
+    }
+};
